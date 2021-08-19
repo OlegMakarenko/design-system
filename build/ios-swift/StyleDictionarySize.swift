2b2ef5455029d429ac0b59a994d628f27b035ff9
Automated Publisher <actions@users.noreply.github.com>
2021-08-19 13:06:35 +0000

    Automated publish: Thu Aug 19 13:06:35 UTC 2021 5fe53e964fb60a6e15b180ad2e6610e95346e349

---
+++ build/ios-swift/StyleDictionarySize.swift
@@ -0,0 +1,131 @@
+
+//
+// StyleDictionarySize.swift
+//
+
+// Do not edit directly
+// Generated on Thu, 19 Aug 2021 13:06:34 GMT
+
+
+import UIKit
+
+public enum StyleDictionarySize {
+    public static let bodyBoldFontSize = "18"
+    public static let bodyBoldFontWeight = "700"
+    public static let bodyBoldLetterSpacing = "0"
+    public static let bodyBoldLineHeight = "150"
+    public static let bodyBoldParagraphIndent = "0"
+    public static let bodyBoldParagraphSpacing = "15"
+    public static let bodyFontSize = "18"
+    public static let bodyFontWeight = "400"
+    public static let bodyHeroFontSize = "20"
+    public static let bodyHeroFontWeight = "400"
+    public static let bodyHeroLetterSpacing = "0"
+    public static let bodyHeroLineHeight = "150"
+    public static let bodyHeroParagraphIndent = "0"
+    public static let bodyHeroParagraphSpacing = "17"
+    public static let bodyLetterSpacing = "0"
+    public static let bodyLineHeight = "150"
+    public static let bodyParagraphIndent = "0"
+    public static let bodyParagraphSpacing = "15"
+    public static let bodySmallFontSize = "15"
+    public static let bodySmallFontWeight = "400"
+    public static let bodySmallLetterSpacing = "0"
+    public static let bodySmallLineHeight = "150"
+    public static let bodySmallParagraphIndent = "0"
+    public static let bodySmallParagraphSpacing = "12"
+    public static let buttonBoundedFontSize = "20"
+    public static let buttonBoundedFontWeight = "700"
+    public static let buttonBoundedLetterSpacing = "2"
+    public static let buttonBoundedLineHeight = "100"
+    public static let buttonBoundedParagraphIndent = "0"
+    public static let buttonBoundedParagraphSpacing = "0"
+    public static let buttonUnboundedFontSize = "24"
+    public static let buttonUnboundedFontWeight = "700"
+    public static let buttonUnboundedLetterSpacing = "2"
+    public static let buttonUnboundedLineHeight = "100"
+    public static let buttonUnboundedParagraphIndent = "0"
+    public static let buttonUnboundedParagraphSpacing = "0"
+    public static let captionFontSize = "18"
+    public static let captionFontWeight = "400"
+    public static let captionLetterSpacing = "0"
+    public static let captionLineHeight = "120"
+    public static let captionParagraphIndent = "0"
+    public static let captionParagraphSpacing = "10"
+    public static let glowBlueOffsetX = 0
+    public static let glowBlueOffsetY = 0
+    public static let glowBlueRadius = 28
+    public static let glowBlueSpread = 0
+    public static let glowFusiaOffsetX = 0
+    public static let glowFusiaOffsetY = 0
+    public static let glowFusiaRadius = 28
+    public static let glowFusiaSpread = 0
+    public static let glowLargeButtonsOffsetX = 0
+    public static let glowLargeButtonsOffsetY = 0
+    public static let glowLargeButtonsRadius = 12
+    public static let glowLargeButtonsSpread = 0
+    public static let glowYellowOffsetX = 0
+    public static let glowYellowOffsetY = 0
+    public static let glowYellowRadius = 28
+    public static let glowYellowSpread = 0
+    public static let h1FontSize = "64"
+    public static let h1FontWeight = "700"
+    public static let h1LetterSpacing = "-2"
+    public static let h1LineHeight = "100"
+    public static let h1ParagraphIndent = "0"
+    public static let h1ParagraphSpacing = "0"
+    public static let h2FontSize = "40"
+    public static let h2FontWeight = "700"
+    public static let h2LetterSpacing = "-2"
+    public static let h2ParagraphIndent = "0"
+    public static let h2ParagraphSpacing = "0"
+    public static let h3FontSize = "24"
+    public static let h3FontWeight = "700"
+    public static let h3LetterSpacing = "0"
+    public static let h3ParagraphIndent = "0"
+    public static let h3ParagraphSpacing = "0"
+    public static let labelFontSize = "18"
+    public static let labelFontWeight = "700"
+    public static let labelLetterSpacing = "0"
+    public static let labelLineHeight = "120"
+    public static let labelParagraphIndent = "0"
+    public static let labelParagraphSpacing = "0"
+    public static let lg992pxCount = 12
+    public static let lg992pxGutterSize = 16
+    public static let lg992pxOffset = 48
+    public static let linkFontSize = "18"
+    public static let linkFontWeight = "400"
+    public static let linkLetterSpacing = "0"
+    public static let linkLineHeight = "150"
+    public static let linkParagraphIndent = "0"
+    public static let linkParagraphSpacing = "15"
+    public static let md576pxCount = 12
+    public static let md576pxGutterSize = 16
+    public static let md576pxOffset = 46
+    public static let subtitleFontSize = "24"
+    public static let subtitleFontWeight = "500"
+    public static let subtitleLetterSpacing = "0"
+    public static let subtitleParagraphIndent = "0"
+    public static let subtitleParagraphSpacing = "0"
+    public static let titleSuperFontSize = "108"
+    public static let titleSuperFontWeight = "700"
+    public static let titleSuperLetterSpacing = "-2"
+    public static let titleSuperLineHeight = "100"
+    public static let titleSuperParagraphIndent = "0"
+    public static let titleSuperParagraphSpacing = "0"
+    public static let titleUltraFontSize = "180"
+    public static let titleUltraFontWeight = "700"
+    public static let titleUltraLetterSpacing = "-2"
+    public static let titleUltraLineHeight = "100"
+    public static let titleUltraParagraphIndent = "0"
+    public static let titleUltraParagraphSpacing = "0"
+    public static let xl1200pxCount = 12
+    public static let xl1200pxGutterSize = 24
+    public static let xl1200pxOffset = 104
+    public static let xm375pxCount = 8
+    public static let xm375pxGutterSize = 8
+    public static let xm375pxOffset = 16
+    public static let xsm320pxCount = 8
+    public static let xsm320pxGutterSize = 8
+    public static let xsm320pxOffset = 16
+}
